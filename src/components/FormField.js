576d7d661d6397764a129160f5fc9003c33c8999
Michael Godshall <michaelgodshall@gmail.com>
2017-03-19 10:05:53 -0700

    Updated Bootstrap classes for FormField

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -3,10 +3,10 @@ import React from 'react';
 // A reusable form field component
 const FormField = (props) => {
   return (
-    <div className={`form-group ${props.meta.touched && props.meta.invalid ? 'has-error': ''}`}>
-      <label htmlFor={props.input.name} className="control-label">{props.label}</label>
+    <div className={`form-group ${props.meta.touched && props.meta.invalid ? 'has-danger': ''}`}>
+      <label htmlFor={props.input.name} className="form-control-label">{props.label}</label>
       <props.element {...props.input} type={props.type} className="form-control" />
-      {props.meta.touched && props.meta.error && <span className="help-block">{props.meta.error}</span>}
+      {props.meta.touched && props.meta.error && <span className="form-control-feedback">{props.meta.error}</span>}
     </div>
   )
 };
